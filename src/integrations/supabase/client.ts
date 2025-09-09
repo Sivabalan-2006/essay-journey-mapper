c135e6c834a4d6194767a2515339bc49c9cc6f09
Sivabalan-2006 <sivabalansivageethan@gmail.com>
2025-09-09 19:19:41 +0530

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,9 +2,8 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = "https://zohtzmomffclsnqbfgov.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpvaHR6bW9tZmZjbHNucWJmZ292Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTczNDU4NDUsImV4cCI6MjA3MjkyMTg0NX0.b81pf4d2URVwbFknOGgbFqAJ4k3WIb741et7pmjvXC4";
-
+const SUPABASE_URL = "import.meta.env.VITE_SUPABASE_URL";
+const SUPABASE_PUBLISHABLE_KEY = "import.meta.env.VITE_SUPABASE_KEY";
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
 
